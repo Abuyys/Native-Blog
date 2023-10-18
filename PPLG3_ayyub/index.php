0cfe2b39e863693eab96ec22383f8356e52ece13
Abuyys <144396961+Abuyys@users.noreply.github.com>
2023-10-18 14:23:17 +0700

    index.php

--- PPLG3_ayyub/index.php
+++ PPLG3_ayyub/index.php
@@ -67,7 +67,7 @@
                         echo 'active';
                     }
                     ?>" data-bs-interval="4000">
-                    <img src="assets\img<?= $row['gambar']; ?>" class="d-block w-100" alt="...">
+                    <img src="assets/img/<?= $row['gambar']; ?>" class="d-block w-100" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h2><?= $row['judul']; ?></h2>
                         <p><?= $row['subjudul']; ?><p>
@@ -88,5 +88,6 @@
     </div>
     <script src="assets\js\jquery.min.js"></script>
     <script src="assets\css\bootstrap.min.css"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
